a864bc998f356a4d653c8c118d623bac107ce342
roodi <igorrud95@gmail.com>
2023-12-12 23:42:11 +0700

    8kyu

---
+++ 8kyu/065.js
@@ -0,0 +1,11 @@
+// Is it even?
+
+
+function isEven(n) {
+    return n % 2 === 0;
+    }
+    
+    console.log(isEven(4)); // Output: true
+    console.log(isEven(5)); // Output: false
+    console.log(isEven(-2)); // Output: true
+    console.log(isEven(3.5)); // Output: false
