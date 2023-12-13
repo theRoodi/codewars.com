3da31e410fb03a549f643b22ccb1d2ee757f03ee
roodi <igorrud95@gmail.com>
2023-12-13 23:44:47 +0700

    8kyu

---
+++ 8kyu/066.js
@@ -0,0 +1,12 @@
+// What is between?
+
+
+function range(a, b) {
+    let result = [];
+    for (let i = a; i <= b; i++) {
+    result.push(i);
+    }
+    return result;
+    }
+    
+    console.log(range(1, 4)); // Output: [1, 2, 3, 4]
