ee2745a2e98da4fb02dd65038d42f20723bd0f2b
roodi <igorrud95@gmail.com>
2023-12-21 00:20:13 +0700

    8kyu

---
+++ 8kyu/069.js
@@ -0,0 +1,9 @@
+
+
+function calculateVolume(length, width, height) {
+    return length * width * height;
+}
+
+console.log(calculateVolume(2, 3, 4)); // Output: 24
+console.log(calculateVolume(5, 10, 7)); // Output: 350
+console.log(calculateVolume(1, 1, 1)); // Output: 1
